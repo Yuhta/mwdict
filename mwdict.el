859d30b854c0add57fef8f6b67ca52cd7678bfb3
Yuhta <gongchuo.lu@gmail.com>
2013-01-06 20:45:13 -0500

    improved output buffer and history list

--- mwdict.el
+++ mwdict.el
@@ -1,23 +1,28 @@
 (defun mwdict-completions (str pred mode)
-  (setq suggestions
-        (delete ""
-                (split-string
-                 (shell-command-to-string (concat "mwdict-suggest '"
-                                                  str
-                                                  "'"))
-                 "\n")))
-  (cond
-   ((null mode)
+  (let ((suggestions
+         (delete ""
+                 (split-string
+                  (shell-command-to-string (concat "mwdict-suggest '"
+                                                   str
+                                                   "'"))
+                  "\n"))))
+
     (cond
-     ((null suggestions) 'nil)
-     ((eq (length suggestions) 1)
-      (if (member str suggestions) 't
-        (car suggestions)))
-     ((and (> (length suggestions) 1) (< (length suggestions) 15))
-      (reduce 'fill-common-string-prefix suggestions))
-     ('t str)))
-   ((eq mode 't) suggestions)
-   ((eq mode 'lambda) (member str suggestions))))
+     ((null mode)
+      (cond
+       ((null suggestions) 'nil)
+       ((eq (length suggestions) 1)
+        (if (member str suggestions) 't
+          (car suggestions)))
+       ((and (> (length suggestions) 1) (< (length suggestions) 15))
+        (reduce 'fill-common-string-prefix suggestions))
+       ('t str)))
+     ((eq mode 't) suggestions)
+     ((eq mode 'lambda) (member str suggestions)))))
+
+
+; Initialize history list
+(setq mwdict-history nil)
 
 (defun mwdict (word &optional num)
   (interactive (list (let ((completion-ignore-case 't))
@@ -27,9 +32,17 @@
                                 "): ")
                         'mwdict-completions
                         nil nil nil
-                        minibuffer-history
+                        'mwdict-history
                         (thing-at-point 'word)))
                      current-prefix-arg))
-  (if (listp num) (shell-command (concat "mwdict '" word "'"))
-    (shell-command (concat "mwdict '" word
-                           "[" (number-to-string num) "]'"))))
+
+  (let ((command (concat "mwdict '" word)))
+    (if (listp num) (shell-command (concat command "'")
+                                   (get-buffer-create "*MWDict*"))
+      (shell-command (concat command
+                             "[" (number-to-string num) "]'")
+                     (get-buffer-create "*MWDict*"))))
+
+  (set-buffer "*MWDict*")
+  (view-mode)
+  (visual-line-mode))
