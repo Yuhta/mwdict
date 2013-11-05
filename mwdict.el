532d61e8eadd58b95c13ec0cd9d3abbb90c69410
Jimmy Lu <gongchuo.lu@gmail.com>
2013-11-04 22:15:44 -0500

    Improve & add fdupes integration

--- mwdict.el
+++ mwdict.el
@@ -1,48 +1,66 @@
+(defvar mwdict-history nil)
+
+(require 'cl)
+
 (defun mwdict-completions (str pred mode)
   (let ((suggestions
          (delete ""
                  (split-string
-                  (shell-command-to-string (concat "mwdict-suggest '"
+                  (shell-command-to-string (concat "mwdict-suggest \""
                                                    str
-                                                   "'"))
+                                                   "\""))
                   "\n"))))
-
     (cond
      ((null mode)
       (cond
-       ((null suggestions) 'nil)
+       ((null suggestions)
+        nil)
        ((eq (length suggestions) 1)
-        (if (member str suggestions) 't
+        (if (member str suggestions)
+            t
           (car suggestions)))
        ((and (> (length suggestions) 1) (< (length suggestions) 15))
         (reduce 'fill-common-string-prefix suggestions))
-       ('t str)))
-     ((eq mode 't) suggestions)
-     ((eq mode 'lambda) (member str suggestions)))))
+       (t
+        str)))
+     ((eq mode t)
+      suggestions)
+     ((eq mode 'lambda)
+      (member str suggestions)))))
 
-
-; Initialize history list
-(setq mwdict-history nil)
+(defun mwdict-process-filter (proc string)
+  "A process filter used in `mwdict'.
+It rolls the point back to the beginning of the buffer after each
+insertion."
+  (let ((old-buffer (current-buffer)))
+    (unwind-protect
+        (let ((inhibit-read-only t))
+          (set-buffer (process-buffer proc))
+          (goto-char (process-mark proc))
+          (insert string)
+          (set-marker (process-mark proc) (point))
+          (goto-char (point-min)))
+      (set-buffer old-buffer))))
 
 (defun mwdict (word &optional num)
   (interactive (list (let ((completion-ignore-case 't))
                        (completing-read
-                        (concat "Word to search (default "
-                                (thing-at-point 'word)
-                                "): ")
+                        (format "Word to search (default %s): "
+                                (thing-at-point 'word))
                         'mwdict-completions
                         nil nil nil
                         'mwdict-history
                         (thing-at-point 'word)))
                      current-prefix-arg))
-
-  (let ((command (concat "mwdict '" word)))
-    (if (listp num) (shell-command (concat command "'")
-                                   (get-buffer-create "*MWDict*"))
-      (shell-command (concat command
-                             "[" (number-to-string num) "]'")
-                     (get-buffer-create "*MWDict*"))))
-
-  (set-buffer "*MWDict*")
+  (let ((buf-name "*MWDict*")
+        (proc-name "mwdict"))
+    (pop-to-buffer (get-buffer-create buf-name))
+    (setq buffer-read-only nil)
+    (erase-buffer)
+    (setq buffer-read-only t)
+    (start-process proc-name buf-name
+                   "mwdict" (concat word (unless (listp num)
+                                           (format "[%d]" num))))
+    (set-process-filter (get-process proc-name) 'mwdict-process-filter))
   (view-mode)
   (visual-line-mode))
