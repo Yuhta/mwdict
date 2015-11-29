57fdd5538f9b3545b8e65e2d6daa3bae6b877038
Jimmy Lu <gongchuo.lu@gmail.com>
2015-11-28 20:41:39 -0500

    Add lein-bin plugin

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.fusesource.jansi/jansi "1.11"]]
+  :plugins [[lein-bin "0.3.5"]]
   :main ^:skip-aot mwdict.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
