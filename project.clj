ccf7aef7b9305453d50254d62156dbf1f7ac01c7
Jimmy Lu <gongchuo.lu@gmail.com>
2016-02-19 23:35:41 -0500

    Upgrade to clojure 1.8

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/Yuhta/mwdict"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.fusesource.jansi/jansi "1.11"]]
   :plugins [[lein-bin "0.3.5"]]
   :main ^:skip-aot mwdict.core
